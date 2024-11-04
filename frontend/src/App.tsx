206b093b880caf0b0e7980fc70ae9c8ed62592a9
Evanson Igiri <evansonigiri@Evansons-MacBook-Air.local>
2024-11-04 16:42:59 +0100

    initial commit and project setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/users')
+      .then(response => setUsers(response.data))
+      .catch(error => console.error('Error fetching users', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map((user: any) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
